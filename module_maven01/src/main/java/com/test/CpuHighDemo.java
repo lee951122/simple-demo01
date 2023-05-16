f3597bdc4eb7e4a89ba17f482840b466cf13f014
lee <lee@lee.com>
2023-05-16 17:48:42 +0800

    Cpu High Demo commit !!!

---
+++ module_maven01/src/main/java/com/test/CpuHighDemo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @author: likai
+ * Date: 2023/5/16 17:47
+ * Description:
+ */
+public class CpuHighDemo {
+    public static void main(String[] args) {
+        while (true) {
+
+        }
+    }
+}
