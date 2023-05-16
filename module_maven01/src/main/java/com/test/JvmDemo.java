b3eebe308291fe418d54ab0445d2902d2398596b
lee <lee@lee.com>
2023-05-16 15:57:02 +0800

    master first commit!

---
+++ module_maven01/src/main/java/com/test/JvmDemo.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @author: likai
+ * Date: 2023/5/15 0:02
+ * Description:
+ */
+public class JvmDemo {
+    public static void main(String[] args) {
+        String s = "112358";
+    }
+}
